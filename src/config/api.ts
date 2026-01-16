b1eb7303d86488c733cdba38aca3f81caa20a5b4
akshatg5 <akshatgirdhar5@gmail.com>
2026-01-16 09:37:54 +0530

    fix : base api

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,2 +1,2 @@
-export const BASE_API_URL = "https://rfp-management-backend.vercel.app";
+export const BASE_API_URL = "https://rfp-management-backend.vercel.app/api";
 // export const BASE_API_URL = "http://localhost:3000";
