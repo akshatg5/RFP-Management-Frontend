4e18d1af25ae926511429bdf25782237ccf4ad1c
akshatg5 <akshatgirdhar5@gmail.com>
2026-01-16 09:29:22 +0530

    prod api

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,2 +1,2 @@
-export const BASE_API_URL = "https://expert-hub-backend.vercel.app";
+export const BASE_API_URL = "https://rfp-management-backend.vercel.app";
 // export const BASE_API_URL = "http://localhost:3000";
